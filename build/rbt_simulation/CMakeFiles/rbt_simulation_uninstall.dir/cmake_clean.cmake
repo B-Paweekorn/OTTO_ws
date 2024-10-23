fbc799fc40996c491ebee0032fb9b9c46c5cbc48
chawrewerwee <122732439+B-Paweekorn@users.noreply.github.com>
2024-10-24 00:54:19 +0700

    add visual

---
+++ build/rbt_simulation/CMakeFiles/rbt_simulation_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rbt_simulation_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/rbt_simulation_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
