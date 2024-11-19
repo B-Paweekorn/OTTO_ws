d5ab299385bf0ebdb4758efb288dab3f7ea80d5b
chawrewerwee <122732439+B-Paweekorn@users.noreply.github.com>
2024-11-19 22:04:47 +0700

    draft kin

--- otto_controller/scripts/kinematics.py
+++ otto_controller/scripts/kinematics.py
@@ -8,18 +8,44 @@ from sensor_msgs.msg import JointState
 class RobotKinematics(Node):
     def __init__(self):
         super().__init__('kinematics')
+        self.dt = 0.001
+        
+        self.timer = self.create_timer(self.dt, self.timer_callback)
         self.create_subscription(JointState,'/joint_states',self.feedback_callback,10)
-
+        self.create_subscription()
         self.l1 = 0.2
         self.l2 = 0.2
+
+        self.kp = 1
+        self.targ = [0.0] * 4
+        self.q_de = [0.0] * 4
         self.joint_state = {}
+    def timer_callback(self):
+        self.q_de += self.controller([self.joint_state["q"][0], self.joint_state["q"][1], self.joint_state["q"][3], self.joint_state["q"][4]]) * self.dt
 
     def forward_kinematics(self, q1, q2):
-        x = self.l1*np.cos(np.deg2rad(45) - q1) - self.l2*np.cos(np.deg2rad(45) + q2 + q1) + 0.03
-        z = self.l1*np.sin(np.deg2rad(45) - q1) + self.l2*np.sin(np.deg2rad(45) + q2 + q1)
+        x = -self.l1*np.cos(np.deg2rad(45) - q1) + self.l2*np.cos(np.deg2rad(45) + q2 + q1)
+        z = -self.l1*np.sin(np.deg2rad(45) - q1) - self.l2*np.sin(np.deg2rad(45) + q2 + q1)
         return x,z
 
-    def inv_jacobian(self, ):
+    def fnc_jacobian(self, q1, q2, q3, q4):
+        J = np.array([[-self.l1*np.sin(np.deg2rad(45) - q1) + self.l2*np.sin(np.deg2rad(45) + q1 + q2), self.l2*np.sin(np.deg2rad(45) + q1 + q2)], 
+                      [self.l1*np.cos(np.deg2rad(45) - q1) + self.l2*np.cos(np.deg2rad(45) + q1 + q2), self.l2*np.cos(np.deg2rad(45) + q1 + q2)],
+                      [-self.l1*np.sin(np.deg2rad(45) - q3) + self.l2*np.sin(np.deg2rad(45) + q3 + q4), self.l2*np.sin(np.deg2rad(45) + q3 + q4)], 
+                      [self.l1*np.cos(np.deg2rad(45) - q3) + self.l2*np.cos(np.deg2rad(45) + q3 + q4), self.l2*np.cos(np.deg2rad(45) + q3 + q4)]])
+        J_inv = np.linalg.inv(J)
+        return J, J_inv
+    
+    def controller(self, curr):
+        if abs(np.linalg.det(self.fnc_jacobian()[0])) < 0.01:
+            print("Singularlity!!")
+            return np.array([0, 0])
+        err = self.targ - curr # 1 X 4
+        v = err * self.kp
+        q_dot = np.matmul(self.fnc_jacobian()[1], np.transpose(v))
+        return q_dot
+
+    def integrate(self, ):
         pass
 
     def feedback_callback(self, msg):
@@ -38,6 +64,7 @@ class RobotKinematics(Node):
             'q': [joint_data[joint] for joint in order],
         }
         print(self.forward_kinematics(self.joint_state["q"][0], self.joint_state["q"][1]))
+
 def main(args=None):
     rclpy.init(args=args)
     node = RobotKinematics()
